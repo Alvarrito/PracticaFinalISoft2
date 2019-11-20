66c13a51be9beac37cc57d106ce967b5d7ed15f3
Alvarrito <Alvaro.Sanchez35@alu.uclm.es>
2019-11-20 15:08:52 +0100

    Primer commit

---
+++ PracticaTaller/src/main/java/PracticaISO/PracticaTaller/App.java
@@ -0,0 +1,13 @@
+package PracticaISO.PracticaTaller;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
