417b9a7d94520515d6837cff9c64307eab0d2a9c
Jairoxd98 <Jairo.Celada@alu.uclm.es>
2019-11-23 17:27:54 +0100

    Añadimos las clases Almacen y Encargo, y modificamos clase Mecanico

---
+++ PracticaTaller/Iteracion3/src/main/java/org/Iteracion3/Almacen.java
@@ -0,0 +1,12 @@
+package org.Iteracion3;
+
+public class Almacen {
+	private int _idAlmacen;
+	private String _pieza;
+	private String _tipo;
+	private boolean _disponible;
+
+	public void MandarPiezasBox() {
+		throw new UnsupportedOperationException();
+	}
+}
