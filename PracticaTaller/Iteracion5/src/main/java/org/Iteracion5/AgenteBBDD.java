96d783d85e6cfcacc49a095ed36030245ebf7f44
Alvarrito <Alvaro.Sanchez35@alu.uclm.es>
2019-11-25 19:07:03 +0100

    Añadida funcion CierraPedido a clase Reserva e ImprimirCuenta a Mecanico

---
+++ PracticaTaller/Iteracion5/src/main/java/org/Iteracion5/AgenteBBDD.java
@@ -0,0 +1,20 @@
+package org.Iteracion5;
+
+public class AgenteBBDD {
+
+	public void Conectar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void Insertar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void Modificar() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void Eliminar() {
+		throw new UnsupportedOperationException();
+	}
+}
