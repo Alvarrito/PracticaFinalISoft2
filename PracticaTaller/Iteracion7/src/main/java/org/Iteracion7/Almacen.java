06ec6214dd16845e90fdc06d1df67c14c6614b0d
SrDomingow <DomingoJose.Jaime@alu.uclm.es>
2019-11-27 08:51:17 +0100

    Acualización del código para software cadena de talleres

---
+++ PracticaTaller/Iteracion7/src/main/java/org/Iteracion7/Almacen.java
@@ -0,0 +1,15 @@
+package org.Iteracion7;
+
+public class Almacen {
+	
+	public JefeMecanico Boss;
+	
+	private int _idAlmacen;
+	private String _pieza;
+	private String _tipo;
+	private boolean _disponible;
+
+	public void MandarPiezasBox() {
+		throw new UnsupportedOperationException();
+	}
+}
---
+++ PracticaTaller/Iteracion7/src/main/java/org/Iteracion7/Encargo.java
@@ -0,0 +1,31 @@
+package org.Iteracion7;
+
+import java.awt.List;
+
+public class Encargo {
+	private int _tInicio;
+	private int _tfin;
+	private List _codigoPiezas;
+	public Mecanico _unnamed_Mecanico_;
+	public Boxes _unnamed_Boxes_;
+
+	public boolean FinalizarEncargo() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void setUnnamed_Mecanico_(Mecanico aUnnamed_Mecanico_) {
+		this._unnamed_Mecanico_ = aUnnamed_Mecanico_;
+	}
+
+	public Mecanico getUnnamed_Mecanico_() {
+		return this._unnamed_Mecanico_;
+	}
+
+	public void setUnnamed_Boxes_(Boxes aUnnamed_Boxes_) {
+		this._unnamed_Boxes_ = aUnnamed_Boxes_;
+	}
+
+	public Boxes getUnnamed_Boxes_() {
+		return this._unnamed_Boxes_;
+	}
+}
